acaeec71d30850ce91dcc483a1342d4531df9576
Romanin <60302782+romanin-rf@users.noreply.github.com>
2023-04-08 21:33:41 +0200

    Add functional from bsi-kivy to bsi-kivymd
    Next:
    - PluginLoader
    - PluginCreator

--- kivybsi/objects.py
+++ kivybsi/objects.py
@@ -1,11 +1,12 @@
 from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.progressbar import MDProgressBar
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.navigationdrawer import MDNavigationDrawerMenu, MDNavigationDrawer, MDNavigationDrawerItem
 from kivy.properties import ColorProperty, StringProperty, NumericProperty
 
-class BSINavigationDrawerClickableItem(MDNavigationDrawerItem):
+class BSINavigationDrawerItem(MDNavigationDrawerItem):
     selected_color = ColorProperty("#b83950")
     ripple_color = ColorProperty("#ffffff")
 
@@ -22,3 +23,19 @@ class BSICenterLabel(MDLabel):
         kwargs["halign"] = kwargs.get("halign", "center")
         
         super().__init__(**kwargs)
+
+class BSIScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.rootbox = MDBoxLayout(
+            orientation='vertical',
+            padding=("0dp", "70dp", "0dp", "0dp")
+        )
+        super().add_widget(self.rootbox)
+    
+    def __repr__(self) -> str: return super().__repr__().replace("<Screen ", f"<{self.__class__.__name__} ")
+    def add_widget(self, widget, *args, **kwargs): self.rootbox.add_widget(widget, *args, **kwargs)
+    def clear_widgets(self, children=None): self.rootbox.clear_widgets(children)
+    def remove_widget(self, widget, *args, **kwargs): self.rootbox.remove_widget(widget, *args, **kwargs)
+    def collide_widget(self, widget) -> bool: return self.rootbox.collide_widget(widget)
--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import sys
 import psutil
 import time
 from threading import Thread
+from rich.console import Console
 # > KivyMD
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivymd.uix.progressbar import MDProgressBar
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.navigationdrawer import MDNavigationDrawerMenu, MDNavigationDrawer, MDNavigationDrawerItem
 # > Kivy
-from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
 from kivy.lang.builder import Builder
-from kivy.properties import ColorProperty, StringProperty, NumericProperty
+from kivy.clock import Clock
 # > Typing
 from typing import Tuple
 # > Дополнительные библеотеки для создания плагинов
@@ -31,6 +32,7 @@ __author__ = "Romanin"
 __email__ = "semina054@gmail.com"
 
 # ! Other
+console = Console()
 LOCAL_DIR_PATH = os.path.dirname(__file__) \
     if os.path.basename(sys.executable).startswith("python") \
     else os.path.dirname(sys.executable)
@@ -43,6 +45,23 @@ ICON_PATH = get_asset_path("bin/icon.png")
 MAIN_UI_PATH = get_asset_path("bin/ui_main.kv")
 PLUGINS_PATH = get_asset_path("plugins")
 
+# ! Functions
+def get_ram_info() -> Tuple[int, int, int, float]:
+    vm = psutil.virtual_memory()
+    return \
+        round((vm.total/1024)/1024), \
+        round(((vm.total-vm.free)/1024)/1024), \
+        round((vm.free/1024)/1024), \
+        round(((vm.total-vm.free)/vm.total)*100,1)
+
+def get_swap_info() -> Tuple[int, int, int, float]:
+    sm = psutil.swap_memory()
+    return \
+        round((sm.total/1024)/1024), \
+        round(((sm.total-sm.free)/1024)/1024), \
+        round((sm.free/1024)/1024), \
+        round(((sm.total-sm.free)/sm.total)*100,1)
+
 # ! Main Class
 class BSI(MDApp):
     def build(self):
@@ -52,10 +71,32 @@ class BSI(MDApp):
         
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Red"
+        self.clock = Clock
         
-        root: MDScreen = Builder.load_file(MAIN_UI_PATH)
+        self.bsi_root: MDScreen = Builder.load_file(MAIN_UI_PATH)
+        self.bsi_msm: MDScreenManager = self.bsi_root.ids["main_screen_manager"]
+        self.bsi_ms: BSIScreen = self.bsi_msm.get_screen("bsi")
 
-        return root
+        return self.bsi_root
+    
+    def update_bsi_ms(self, dt) -> None:
+        cpu_load = round(psutil.cpu_percent())
+        ram_busy = get_ram_info()
+        swap_busy = get_swap_info()
+        
+        self.bsi_root.ids["ProgressBarCPULoad"].value = cpu_load
+        self.bsi_root.ids["LabelCPULoad"].text = f"{cpu_load} %"
+        
+        self.bsi_root.ids["ProgressBarRAMBusy"].value = ram_busy[3]
+        self.bsi_root.ids["LabelRAMBusy"].text = f"{ram_busy[1]} MB ({ram_busy[3]} %)"
+        
+        self.bsi_root.ids["ProgressBarSWAPBusy"].value = swap_busy[3]
+        self.bsi_root.ids["LabelSWAPBusy"].text = f"{swap_busy[1]} MB ({swap_busy[3]} %)"
+    
+    def on_start(self) -> None:
+        self.bsi_root.ids["LabelRAMTotal"].text = f"{get_ram_info()[0]} MB"
+        self.bsi_root.ids["LabelSWAPTotal"].text = f"{get_swap_info()[0]} MB"
+        self.clock.schedule_interval(self.update_bsi_ms, 0.5)
 
 # ! Start
 if __name__ == "__main__":
